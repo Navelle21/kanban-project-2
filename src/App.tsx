02d0587afa7987c1f62f0c08241b5f393b462bb7
Fachri <fachrify@gmail.com>
2024-05-14 12:02:53 +0700

    Add react router and side menu layout

---
+++ src/App.tsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import {createBrowserRouter, RouterProvider} from 'react-router-dom'
+import SideMenuLayout from './layouts/SideMenuLayouts'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <SideMenuLayout />,
+    children: [
+      {
+        path: '/',
+        element: (
+          <div style={{ display: 'flex' }}>
+            <h1>Home</h1>
+          </div>
+        ),
+      },
+      {
+        path: 'task-list',
+        element: (
+          <div style={{ display: 'flex' }}>
+            <h1>Task List</h1>
+          </div>
+        ),
+      },
+      {
+        path: 'task-progress',
+        element: (
+          <div style={{ display: 'flex' }}>
+            <h1>Task Progress</h1>
+          </div>
+        ),
+      },
+    ]
+  }
+])
+
+function App(): JSX.Element {
+  return <RouterProvider router={router} />
+}
+
+export default App
